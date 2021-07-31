3977cf11ec10b2b9385eb5099eeed24c9e03342f
Xavier Ouimet <x.ouimet@gmail.com>
2021-07-31 16:25:42 -0400

    Fix encoder handler

--- rover_hardware_proxy/scripts/rover_speed_encoder.py
+++ rover_hardware_proxy/scripts/rover_speed_encoder.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 import numpy as np
+import time
 import threading
+from threading import Thread
 import rospy
 from std_msgs.msg import Float32MultiArray
 
@@ -9,14 +11,51 @@ import RPi.GPIO as GPIO
 
 VELOCITY_PINS = {
     "front_left_A"    : 29,
-    "front_left_B"    : 32
+    "front_left_B"    : 23
 }
 
-# def encoderMessageReceived():
-#     """ Every time a new time stamp is received, the velocity is calculated """
-#     time = np.datetime64('now')
-#     if GPIO.input(input_pin_num):
-#         detected_time = np.datetime64('now')
+class EncodingPublisher(Thread):
+    def __init__(self, topic_name, publisher_queue_size = 10, publish_interval = 2, time_history = [], forward = []):
+        self.time_history = time_history
+        self.forward = forward
+        self.publish_interval = publish_interval
+
+        self.publisher = rospy.Publisher(
+            topic_name,
+            Float32MultiArray,
+            queue_size=publisher_queue_size
+        )
+
+        self.schedule_next_publication()
+
+    def schedule_next_publication(self):
+        timer = threading.Timer(self.publish_interval, self.publish_velocity)
+        timer.daemon = True
+        timer.start()
+    
+    def publish_velocity(self):
+        velocity_message = Float32MultiArray()
+
+        derivative = []
+        if len(self.time_history) <= 1:
+            velocity_message.data.append(0)
+        else:
+            for i in range(len(self.time_history)-1):
+                delta_time_sec = self.time_history[i+1] - self.time_history[i]
+                if delta_time_sec == 0:
+                    continue
+                derivative.append(self.forward[i+1] * 2 * (np.pi / 180) / delta_time_sec)
+            
+            velocity_message.data.append(len(derivative) == 0 if 0 else sum(derivative) / len(derivative))
+
+        # Publish the velocity array into the topic
+        self.publisher.publish(velocity_message)
+        self.time_history = []
+        self.forward = []
+
+        print(velocity_message.data)
+
+        self.schedule_next_publication()
 
 
 def initialize_gpio():
@@ -31,30 +70,18 @@ def initialize_gpio():
 def checkDirection(A_UP, B_UP):
     FORWARD = 0
     # Loop to determine the direction of rotation
-    if (A_UP == 1) && (B_UP == 0):
-        FORWARD = 0
-    elif (A_UP == 0) && (B_UP == 1):
-        FORWARD = 0
-    elif (A_UP == 1) && (B_UP == 1):
+    if (A_UP == 1) and (B_UP == 0):
         FORWARD = 1
-    elif (A_UP == 0) && (B_UP == 0):
+    elif (A_UP == 0) and (B_UP == 1):
+        FORWARD = 0
+    elif (A_UP == 1) and (B_UP == 1):
+        FORWARD = 0
+    elif (A_UP == 0) and (B_UP == 0):
         FORWARD = 1
+
+    print("Is forward: %s" % FORWARD)
 
     return FORWARD
-
-def publisher(time, velocity_message, forward):
-    derivative = []
-    if len(time) <= 1:
-        velocity_message.data.append(0)
-    else:
-        for i in range(len(time)-1):
-            delta_time_ns = (time[i+1] - time[i]) / np.timedelta64(1, 'ns')
-            derivative.append(forward[i+1] * np.pi / delta_time_ns)
-        velocity_message.data.append(sum(derivative) / len(derivative))
-
-    # Publish the velocity array into the topic
-    encoder_Publisher.publish(velocity_message)
-    print(velocity_message.data)
 
 def main():
     initialize_gpio()
@@ -62,7 +89,7 @@ def main():
     rospy.init_node('rover_velocity_encoder', anonymous=True)
 
     # Publisher to publish the encoder velocity values
-    encoder_Publisher = rospy.Publisher('/velocity', Float32MultiArray, queue_size=10)
+    encodingHandler = EncodingPublisher('/velocity')
 
     # Length of the wait time before calculating velocity
     LENGTH_TIME_ARRAY = 10
@@ -74,50 +101,30 @@ def main():
     A_UP = 0
     B_UP = 0
 
-    # Average time before calculating velocity despite time array not full
-    MAX_TIME_BEFORE_PUBLISH = 15  # seconds
-
     while not rospy.is_shutdown():
-        time = []
-        forward = []
-        velocity_message = Float32MultiArray()
+        #print("Channel A: %s, channel B: %s" % (GPIO.input(VELOCITY_PINS["front_left_A"]), GPIO.input(VELOCITY_PINS["front_left_B"])))
 
-        # To reset the states whent he signal turn off
-        if GPIO.input(29):
+        # To reset the states when the signal turn off
+        if GPIO.input(VELOCITY_PINS["front_left_A"]) and (STATE_A == 1):
             STATE_A = 0
             A_UP = 0
-        elif GPIO.input(32):
+        elif GPIO.input(VELOCITY_PINS["front_left_B"]) and (STATE_B == 1):
             STATE_B = 0
             B_UP = 0
 
         # Loop for recording time stamps when a magnet is detected
-        if (GPIO.input(29) == False) && (STATE_A == 0):
+        if (GPIO.input(VELOCITY_PINS["front_left_A"]) == False) and (STATE_A == 0):
             STATE_A = 1
             A_UP = 1
             FORWARD = checkDirection(A_UP, B_UP)
-            time.append(np.datetime64('now'))
+            encodingHandler.time_history.append(time.time())
             if FORWARD:
-                forward.append(1)
+                encodingHandler.forward.append(1)
             else:
-                forward.append(-1)
-        elif (GPIO.input(32) == False) && (STATE_B == 0):
+                encodingHandler.forward.append(-1)
+        elif (GPIO.input(VELOCITY_PINS["front_left_B"]) == False) and (STATE_B == 0):
             STATE_B = 1
             B_UP = 1
-            FORWARD = checkDirection(A_UP, B_UP)
-            time.append(np.datetime64('now'))
-            if FORWARD:
-                forward.append(1)
-            else:
-                forward.append(-1)
-
-
-
-        # if len(time) == LENGTH_TIME_ARRAY :
-        #     publisher(time, velocity_message)
-
-        # If the MAX_TIME_BEFORE_PUBLISH has been reached, publish anyway
-        start_time = threading.Timer(MAX_TIME_BEFORE_PUBLISH, publisher(time, velocity_message, forward))
-
 
 if __name__ == '__main__':
     try:
