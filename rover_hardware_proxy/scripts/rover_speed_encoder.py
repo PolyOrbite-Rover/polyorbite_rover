285f70d7775e2e32e45041f0e95ecca4cb89800d
Thinking-Dragon <cfmgp@hotmail.com>
2021-08-01 19:55:00 -0400

    Make encoder work with multiple motors

--- rover_hardware_proxy/scripts/rover_speed_encoder.py
+++ rover_hardware_proxy/scripts/rover_speed_encoder.py
@@ -10,14 +10,56 @@ from std_msgs.msg import Float32MultiArray
 import RPi.GPIO as GPIO
 
 VELOCITY_PINS = {
-    "front_left_A"    : 29,
-    "front_left_B"    : 23
+    "front_left": {
+        "A": 29,
+        "B": 23
+    },
+    "middle_left": {
+        "A": 24,
+        "B": 22
+    },
+    "rear_left": {
+        "A": 21,
+        "B": 19
+    },
+    "front_right": {
+        "_A": 18,
+        "_B": 16
+    },
+    "middle_right": {
+        "A": 15,
+        "B": 13
+    },
+    "rear_right": {
+        "A": 11,
+        "B": 12
+    }
 }
+
+def checkDirection(A_UP, B_UP):
+    FORWARD = 0
+    # Loop to determine the direction of rotation
+    if (A_UP == 1) and (B_UP == 0):
+        FORWARD = 1
+    elif (A_UP == 0) and (B_UP == 1):
+        FORWARD = 0
+    elif (A_UP == 1) and (B_UP == 1):
+        FORWARD = 0
+    elif (A_UP == 0) and (B_UP == 0):
+        FORWARD = 1
+
+    return FORWARD
 
 class EncodingPublisher(Thread):
     def __init__(self, topic_name, publisher_queue_size = 10, publish_interval = 2, time_history = [], forward = []):
         self.time_history = time_history
         self.forward = forward
+
+        self.STATE_A = 0
+        self.STATE_B = 0
+        self.A_UP = 0
+        self.B_UP = 0
+
         self.publish_interval = publish_interval
 
         self.publisher = rospy.Publisher(
@@ -57,6 +99,29 @@ class EncodingPublisher(Thread):
 
         self.schedule_next_publication()
 
+    def update(front_leftpin_A, pin_B):
+        # To reset the states when the signal turn off
+        if GPIO.input(pin_A) and (self.STATE_A == 1):
+            self.STATE_A = 0
+            self.A_UP = 0
+        elif GPIO.input(pin_B) and (self.STATE_B == 1):
+            self.STATE_B = 0
+            self.B_UP = 0
+
+        # Loop for recording time stamps when a magnet is detected
+        if (GPIO.input(pin_A) == False) and (self.STATE_A == 0):
+            self.STATE_A = 1
+            self.A_UP = 1
+            FORWARD = checkDirection(self.A_UP, self.B_UP)
+            encodingHandler.time_history.append(time.time())
+            if FORWARD:
+                encodingHandler.forward.append(1)
+            else:
+                encodingHandler.forward.append(-1)
+        elif (GPIO.input(pin_B) == False) and (self.STATE_B == 0):
+            self.STATE_B = 1
+    rospy.init_node('rover_velocity_encod
+
 
 def initialize_gpio():
     """
@@ -67,20 +132,6 @@ def initialize_gpio():
     for pin in VELOCITY_PINS.values():
         GPIO.setup(pin, GPIO.IN)
 
-def checkDirection(A_UP, B_UP):
-    FORWARD = 0
-    # Loop to determine the direction of rotation
-    if (A_UP == 1) and (B_UP == 0):
-        FORWARD = 1
-    elif (A_UP == 0) and (B_UP == 1):
-        FORWARD = 0
-    elif (A_UP == 1) and (B_UP == 1):
-        FORWARD = 0
-    elif (A_UP == 0) and (B_UP == 0):
-        FORWARD = 1
-
-    return FORWARD
-
 def main():
     initialize_gpio()
 
@@ -88,41 +139,47 @@ def main():
 
     # Publisher to publish the encoder velocity values
     encodingHandler = EncodingPublisher('/velocity')
-
-    # Length of the wait time before calculating velocity
-    LENGTH_TIME_ARRAY = 10
-
-    ## Loop to calculate the velocity every time a time measure is made
-    # Variable to identify if the signal is on or off and to determine the direction
-    STATE_A = 0
-    STATE_B = 0
-    A_UP = 0
-    B_UP = 0
+    encodingHandlers = {
+        "front_left": EncodingPublisher('/velocity'),
+        "middle_left": EncodingPublisher('/velocity'),
+        "rear_left": EncodingPublisher('/velocity'),
+        "front_right": EncodingPublisher('/velocity'),
+        "middle_right": EncodingPublisher('/velocity'),
+        "rear_right": EncodingPublisher('/velocity')
+    }
 
     while not rospy.is_shutdown():
         #print("Channel A: %s, channel B: %s" % (GPIO.input(VELOCITY_PINS["front_left_A"]), GPIO.input(VELOCITY_PINS["front_left_B"])))
+        encodingHandlers['front_left'].update(
+            VELOCITY_PINS['front_left']['A'],
+            VELOCITY_PINS['front_left']['B']
+        )
 
-        # To reset the states when the signal turn off
-        if GPIO.input(VELOCITY_PINS["front_left_A"]) and (STATE_A == 1):
-            STATE_A = 0
-            A_UP = 0
-        elif GPIO.input(VELOCITY_PINS["front_left_B"]) and (STATE_B == 1):
-            STATE_B = 0
-            B_UP = 0
+        encodingHandlers['middle_left'].update(
+            VELOCITY_PINS['middle_left']['A'],
+            VELOCITY_PINS['middle_left']['B']
+        )
 
-        # Loop for recording time stamps when a magnet is detected
-        if (GPIO.input(VELOCITY_PINS["front_left_A"]) == False) and (STATE_A == 0):
-            STATE_A = 1
-            A_UP = 1
-            FORWARD = checkDirection(A_UP, B_UP)
-            encodingHandler.time_history.append(time.time())
-            if FORWARD:
-                encodingHandler.forward.append(1)
-            else:
-                encodingHandler.forward.append(-1)
-        elif (GPIO.input(VELOCITY_PINS["front_left_B"]) == False) and (STATE_B == 0):
-            STATE_B = 1
-            B_UP = 1
+        encodingHandlers['rear_left'].update(
+            VELOCITY_PINS['rear_left']['A'],
+            VELOCITY_PINS['rear_left']['B']
+        )
+
+        encodingHandlers['front_right'].update(
+            VELOCITY_PINS['front_right']['A'],
+            VELOCITY_PINS['front_right']['B']
+        )
+
+        encodingHandlers['middle_right'].update(
+            VELOCITY_PINS['middle_right']['A'],
+            VELOCITY_PINS['middle_right']['B']
+        )
+
+        encodingHandlers['rear_right'].update(
+            VELOCITY_PINS['rear_right']['A'],
+            VELOCITY_PINS['rear_right']['B']
+        )
+        
 
 if __name__ == '__main__':
     try:
